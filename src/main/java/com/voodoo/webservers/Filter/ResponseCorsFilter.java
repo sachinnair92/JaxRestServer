cf9416508dbbdb89bf14057303e24178f5f66935
sachinnair92 <sachinnair92@gmail.com>
2016-05-25 22:31:31 +0530

    first commit

---
+++ src/main/java/com/voodoo/webservers/Filter/ResponseCorsFilter.java
@@ -0,0 +1,26 @@
+package com.voodoo.webservers.Filter;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+
+/**
+ * Created by voodoo on 25/5/16.
+ */
+
+@Provider
+public class ResponseCorsFilter implements ContainerResponseFilter {
+
+
+    @Override
+    public void filter(ContainerRequestContext containerRequestContext, ContainerResponseContext containerResponseContext) throws IOException {
+
+        containerResponseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
+        containerResponseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
+        containerResponseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
+        containerResponseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+        containerResponseContext.getHeaders().add("Access-Control-Max-Age", "1209600");
+    }
+}
